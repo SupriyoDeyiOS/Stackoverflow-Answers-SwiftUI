d80a7000e9c4ee5da3ad375a869b36a04a6e5351
Supriyo Dey <supriyodeyiosdev@gmail.com>
2023-08-29 16:10:04 +0530

    Initial Commit

---
+++ StackOverFlowSwiftUI/StackOverFlowSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackOverFlowSwiftUIApp.swift
+//  StackOverFlowSwiftUI
+//
+//  Created by Yagnesh Bagrodia on 29/08/23.
+//
+
+import SwiftUI
+
+@main
+struct StackOverFlowSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
